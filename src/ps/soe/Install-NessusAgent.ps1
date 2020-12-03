4cb4fa3317497fcef08f9b1a33ec5abd1a2876c7
Pat Hermens <p@hermens.com.au>
2020-12-03 18:55:55 +1100

    Updated Nessus script to use EnvVars & S3

--- src/ps/soe/Install-NessusAgent.ps1
+++ src/ps/soe/Install-NessusAgent.ps1
@@ -3,7 +3,7 @@
 
 .VERSION 1.0
 
-.GUID c4783a8e-7d4d-48ec-9ace-440f3e956809
+.GUID 8c32d3e2-ac85-4d4b-afd4-9fbc97316419
 
 .AUTHOR pat.hermens@slalom.com
 
@@ -23,13 +23,13 @@
  then executes it in the background (via 'msiexec /i /qn') to install & configure the agent.
 
 .PARAMETER NessusKey
- [MANDATORY] The key with enough permissions to add this agent to the requested group
+ The key with enough permissions to add this agent to the requested group
  
 .PARAMETER NessusGroups
- [OPTIONAL] The name of the groups that this Nessus Agent should be subscribed to
+ The name of the groups that this Nessus Agent should be subscribed to
 
-.PARAMETER NessusServer
- [OPTIONAL] The URL of the Tenable server that hosts your environment
+.PARAMETER NessusInstanceName
+ The identifier that you want to use for this instance in Tenable Cloud
 
 .INPUTS
  None
@@ -43,63 +43,100 @@
 
 .EXAMPLE
  PS> # When using command-line parameters
- PS> Install-NessusAgent -NessusKey "long-api-key" -NessusGroups "AU Servers" -NessusServer "cloud.tenable.com"
+ PS> Install-NessusAgent -NessusKey "long-api-key" -NessusGroups "AU Servers" -NessusInstanceName "TestInstance"
+
+ .EXAMPLE
+ PS> # When using alias'
+ PS> Install-NessusAgent -NessusKey "long-api-key" -G "AU Servers" -N "TestInstance" 
 
 #> 
 function Install-NessusAgent {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)][string]$NessusKey,
+        [Alias("K")][string]$NessusKey,
         [Alias("G")][string]$NessusGroups = "AU Servers",
-        [Alias("S")][string]$NessusServer = "cloud.tenable.com"
+        [Alias("N")][string]$NessusInstanceName
     )
     
     BEGIN {
         Write-Verbose "=> '$PSCommandPath' has started.";
 
+        # Fall back to ENV VARs if available 
+        if (Get-ChildItem -Path "ENV:Nessus*") {
+            if ((-not $NessusKey) -and $ENV:NessusKey) {
+                $NessusKey = $ENV:NessusKey
+                Write-Verbose "Nessus Key loaded from matching environment variable."
+            }
+            if ((-not $NessusGroups) -and $ENV:NessusGroups) {
+                $NessusGroups = $ENV:NessusGroups
+                Write-Verbose "Nessus Groups loaded from matching environment variable."
+            }
+            if ((-not $NessusInstanceName) -and $ENV:NessusInstanceName) {
+                $NessusInstanceName = $ENV:NessusInstanceName
+                Write-Verbose "Nessus InstanceName loaded from matching environment variable."
+            }
+        }
+
         # Validate/assign parameters
+        if (-not $NessusKey) {
+            throw [System.ArgumentNullException] "NessusKey"
+        }
         if (-not $NessusGroups) {
             throw [System.ArgumentNullException] "NessusGroups"
         }
-        if (-not $NessusServer) {
-            throw [System.ArgumentNullException] "NessusServer"
+        if (-not $NessusInstanceName) {
+            throw [System.ArgumentNullException] "NessusInstanceName"
         }
 
         # Assign global variables
-        $NessusAgentDownloadURL = "https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/11782/download?i_agree_to_tenable_license_agreement=true"
-        $PublishedNessusAgentHash = "d7cac60d8b8fb3cca566ae14cc937718da7eac711b282416c661af7afe13c7a5".ToUpper()
         $DownloadedNessusAgent = "NessusAgent.msi"
+        $InstalledNessusExe = "C:\Program Files\Tenable\Nessus Agent\nessuscli.exe"
         $LoggingReplacement = "***REMOVED***"
+        $S3BucketName = $ENV:PenguinInfraBucketName
+        $S3BucketFolder = "installers"
 
         # Download prerequisite packages
-        if (Test-Path $DownloadedNessusAgent) {
-            Write-Warning "Previously downloaded Nessus Agent located ('$(Resolve-Path $DownloadedNessusAgent)'); Skipping download!"
+        if (Test-Path $InstalledNessusExe) {
+            Write-Information "Nessus Agent executable is already installed! Skipping download."
         } else {
-            Invoke-WebRequest -Uri $NessusAgentDownloadURL -OutFile $DownloadedNessusAgent
-        }
-
-        $DownloadedNessusAgentHash = (Get-FileHash -Algorithm SHA256 -Path $DownloadedNessusAgent).Hash;
-        Write-Verbose "Located '$(Resolve-Path $DownloadedNessusAgent)' - HASH: $DownloadedNessusAgentHash";
-
-        if ($DownloadedNessusAgentHash -ne $PublishedNessusAgentHash) {
-            throw "NessusAgent file hash is different from expected!`n`n" +`
-                    "EXPECTED: $PublishedNessusAgentHash`n`n" +`
-                    "RECEIVED: $DownloadedNessusAgentHash`n`n";
-        } else {
-            Write-Verbose "'$(Resolve-Path $DownloadedNessusAgent)': SHA256 Hash meets expectation."
+            if (Test-Path $DownloadedNessusAgent) {
+                Write-Warning "Previously downloaded Nessus Agent located ('$(Resolve-Path $DownloadedNessusAgent)'); Skipping download!"
+            } else {
+                # Fetch from S3?
+                if (-not $S3BucketName) {
+                    throw "S3BucketName for Nessus Installer was not specified. Please populate the 'PenguinInfraBucketName' environment variable!"
+                }
+                if (Get-S3Object -BucketName $S3BucketName) {
+                    Read-S3Object -BucketName $S3BucketName -KeyPrefix $S3BucketFolder -File $DownloadedInstaller
+                    log -msg "The contents of the '$S3BucketFolder' folder in the '$S3BucketName' S3Bucket have been downloaded to '$(Resolve-Path $DownloadedInstaller)'."
+                } else {
+                    throw "S3Bucket '$S3BucketName' could not be read. Please check permissions if it exists!"
+                }
+            }
         }
     }
 
     PROCESS {
         try {
 
-            $arguments = "/i $DownloadedNessusAgent /passive /norestart /qn /LAME "".\install_nessusagent.log"" NESSUS_GROUPS=""$NessusGroups"" NESSUS_SERVER=""$NessusServer"" NESSUS_KEY=""$LoggingReplacement"""
-            Write-Verbose "Executing: 'msiexec $arguments'"
-            $arguments = $arguments.Replace($LoggingReplacement, $NessusKey)
+            # Installation
+            if (Test-Path $InstalledNessusExe) {
+                Write-Information "Nessus Agent executable is already installed! Skipping installation."
+            } else {
+                $arguments = "/i $DownloadedNessusAgent /passive /norestart /qn /LAME "".\install_nessusagent.log"" NESSUS_GROUPS=""$NessusGroups"" NESSUS_KEY=""$LoggingReplacement"""
+                Write-Verbose "Executing: 'msiexec $arguments'"
+                $arguments = $arguments.Replace($LoggingReplacement, $NessusKey)
+                Start-Process "msiexec" -ArgumentList $arguments -Wait
+            }
 
-            Start-Process "msiexec" -ArgumentList $arguments -Wait
+            # Configuration: 
+            # 'agent link --cloud --key=$NessusKey --groups=$NessusGroups --name=$NessusInstanceName'
+            $arguments = "agent link --cloud --key=$NessusKey --groups=$NessusGroups --name=$NessusInstanceName'"
+            Write-Verbose "Executing: '$InstalledNessusExe $arguments'"
+            Start-Process $InstalledNessusExe -ArgumentList $arguments -Wait
 
-            Write-Output "[✓] All done. Nessus Agent is installed & configured."
+            Write-Output "All done. Nessus Agent is installed and configured."
+
         }
         catch {
             Write-Error "An error occurred that could not be automatically resolved:"
