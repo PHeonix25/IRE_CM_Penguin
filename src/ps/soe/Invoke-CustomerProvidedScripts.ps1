b2bbc74439da437f25af8797702981a652e60e43
Pat Hermens <p@hermens.com.au>
2020-12-30 16:25:17 +1100

    Remove IIS configuration from pre-deployment stage

--- src/ps/soe/Invoke-CustomerProvidedScripts.ps1
+++ src/ps/soe/Invoke-CustomerProvidedScripts.ps1
@@ -75,60 +75,6 @@ function Invoke-CustomerProvidedScripts {
                 . "$script";
                 Write-Verbose "Execution of configuration script '$($script.FullName)' completed.";
             }
-
-            # Create self-signed certificate & add it to root cert store
-            $DomainName = "*.covermore.co.uk";
-            New-SelfSignedCertificate -DnsName $DomainName -CertStoreLocation "cert:\LocalMachine\My"
-            Write-Output "Created new self-signed certificate for DnsName '$DomainName'."
-            $cert = (Get-ChildItem "cert:\LocalMachine\My" | Where-Object Subject -like "$DomainName*").Thumbprint
-            Write-Output "Located certificate with thumbprint '$cert'. Moving to Root store."
-            Move-Item "cert:\LocalMachine\My\$cert" -Destination "cert:\LocalMachine\Root\"
-            Write-Output "Certificate ('$cert') has been moved to the Root store.";
-
-            Import-Module WebAdministration;
-            $hostsFilePath = "$($ENV:WinDir)\system32\Drivers\etc\hosts"
-            $hostsFile = Get-Content $hostsFilePath;
-            foreach ($site in $(Get-ChildItem IIS:\Sites | Where-Object Name -ne "Default Web Site")) {
-                $siteName = $site.Name;
-
-                # Add HTTPS to each site (except the 'Default Web Site')
-                New-WebBinding -Name "$siteName" -IpAddress "*" -Protocol "https" -Port 443 -HostHeader "$siteName"
-                Write-Output "New HTTPS binding has been added to '$siteName'."
-                # For some reason the return object for 'new-webbinding' is not the same as the return object from 'get-webbinding'!?
-                (Get-WebBinding -Name "$siteName" -Protocol "https" -Port 443 -HostHeader "$siteName").AddSslCertificate($cert, "Root")
-                Write-Output "HTTPS binding for '$siteName' has been updated to use the self-signed certificate '$cert'.";
-
-                # Add hostsfile entries for each site (to enable local access without DNS resolution)
-                $header = "# Cover-More local websites:"
-                if ($hostsFile -notcontains $header)  {
-                    Add-Content -Path $hostsFilePath -Encoding "utf8" -Value "`n$header ";
-                }
-                $escapedSiteName = [Regex]::Escape($siteName)
-                $hostEntry = "127.0.0.1`t$siteName";
-                if ($hostsFile -match ".*\s+$escapedSiteName.*")  {
-                    Write-Warning "Not able to add '$hostEntry' to hostfile. Entry already exists."
-                } else {
-                    Write-Output "Adding '$hostEntry' to hostsfile..."
-                    Add-Content -Path $hostsFilePath -Encoding "utf8" -Value $hostEntry;
-                    Write-Host "Success! '$hostEntry' has been added to the hostsfile."
-                }
-            }
-
-            # Add Forms auth for *-crm.covermore.co.uk
-            foreach ($site in $(Get-ChildItem IIS:\Sites | Where-Object Name -like "*-crm.covermore.co.uk")) {
-                $config = Get-WebConfiguration -Filter "system.web/authentication" -PSPath "IIS:\" -Location "$($site.Name)"
-                $config.mode = "Forms"
-                $config | Set-WebConfiguration -Filter "system.web/authentication" -PSPath "IIS:\" -Location "$($site.Name)"
-                Write-Output "Enabled Forms Authentication for '$($site.Name)'."
-            }
-            # Add Windows auth & disable anonymous auth for *-login.covermore.co.uk
-            foreach ($site in $(Get-ChildItem IIS:\Sites | Where-Object Name -like "*-login.covermore.co.uk")) {
-                Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/anonymousAuthentication" -Name "Enabled" -Value "False" -PSPath "IIS:\" -Location "$($site.Name)"
-                Write-Output "Disabled Anonymous Authentication for '$($site.Name)'."
-                Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/windowsAuthentication" -Name "Enabled" -Value "True" -PSPath "IIS:\" -Location "$($site.Name)"
-                Write-Output "Enabled Windows Authentication for '$($site.Name)'."
-            }
-            
         }
         catch {
             Write-Error "An error occurred that could not be automatically resolved: $_"
